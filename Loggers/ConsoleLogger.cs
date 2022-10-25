790d4ad68441da0e37287f3fd90194808a88d69f
Amol Jawalkar <aj142018@renishaw.com>
2022-10-25 10:20:36 +0530

    Added loggers project for testing Ioc functionality

---
+++ Loggers/ConsoleLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loggers
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void WriteError(string message)
+        {
+           Console.WriteLine( "Error : " + message);
+        }
+
+        public void WriteInformation(string message)
+        {
+            Console.WriteLine("Information : " + message);
+        }
+
+        public void WriteWarning(string message)
+        {
+            Console.WriteLine("Warning : " + message);
+        }
+    }
+}
---
+++ Loggers/ILogger.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Loggers
+{
+    public interface ILogger
+    {
+        void WriteInformation(string message);
+        void WriteWarning(string message);
+        void WriteError(string message);
+    }
+}
