bf9864b3e987afea3ed2b86270bb332da4247e11
Amol Jawalkar <aj142018@renishaw.com>
2022-10-25 10:29:40 +0530

    Created sample application to demo Ioc feature

---
+++ IocApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using Ioc;
+using Loggers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = BootStrap();
+            var logger = container.Resolve<ILogger>();
+
+            logger.WriteInformation("First message using Ioc");
+        }
+
+
+        private static Container BootStrap()
+        {
+            var iocContainer = new Container();
+            iocContainer.Register<ILogger, ConsoleLogger>();
+            return iocContainer;
+        }
+    }
+}
